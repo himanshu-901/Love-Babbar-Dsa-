a5c8f58724057e07c81f57f23a48433cb34b1934
Himanshu Sharma <97234895+himanshu-901@users.noreply.github.com>
2022-08-27 12:03:53 +0530

    Update dectobin2.cpp

--- Lecture 6: Binary & Decimal Number System/dectobin2.cpp
+++ Lecture 6: Binary & Decimal Number System/dectobin2.cpp
@@ -1,1 +1,20 @@
+// decimal to biniary conversion using Bitwise operator - (>>,&)
+//Approach no - 2
 
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main() 
+{
+    int n,ans=0,i=0;
+    cin>>n;
+    while(n!=0){
+      int bit = n&1;
+      ans = (pow(10,i)*bit) + ans ;
+      n = n>>1;
+      i++;
+    }
+    cout<<ans;
+    return 0;
+}
