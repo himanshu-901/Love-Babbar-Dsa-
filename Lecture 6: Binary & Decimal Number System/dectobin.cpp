85b81aff007bddf5bbe7749819c20aeab60050dc
Himanshu Sharma <97234895+himanshu-901@users.noreply.github.com>
2022-08-27 11:46:34 +0530

    Update dectobin.cpp

--- Lecture 6: Binary & Decimal Number System/dectobin.cpp
+++ Lecture 6: Binary & Decimal Number System/dectobin.cpp
@@ -1,1 +1,23 @@
+// decimal to biniary conversion using arithmetic operators - (% and /)
 
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n,ans=0,i=0;
+    cin>>n;
+    int array[n];
+    
+    while(n!=0)
+    {
+      array[i] = n%2;
+      n = n/2;
+      i++;
+    }
+    for(int j=i-1;j>=0;j--)
+    {
+      cout<<array[j];
+    }
+    return 0;
+}
