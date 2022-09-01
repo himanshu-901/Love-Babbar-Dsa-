7c64db19b47b5e820eccd55b6890f45c4963336f
Himanshu Sharma <97234895+himanshu-901@users.noreply.github.com>
2022-09-01 16:28:03 +0530

    Create switch.cpp

---
+++ Lecture 7: Switch & functions/switch/switch.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+  /*
+  int num = 2;
+  switch(num){
+    case 1: cout<<"first"<<endl;
+            break;
+    case 2: cout<<"second"<<endl;
+            break;
+    default: cout<<"not find in any case"<<endl;
+  }
+  cout<<endl;
+  */
+
+  /*
+  int a,b;
+  cin>>a>>b;
+  char op;
+  cin>>op;
+  switch(op){
+    case '+': cout<<a+b<<endl;
+          break;
+    case '-': cout<<a-b<<endl;
+          break;
+    case '*' : cout<<a*b<<endl;
+          break;
+    case '/' : cout<<a/b<<endl;
+          break;
+    default: cout<<"invalid operation"<<endl;
+    }
+  cout<<endl;
+  }
+  */
+
+  
