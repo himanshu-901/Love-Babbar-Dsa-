6890390bfc9686a791e44248ccbf92b8f305c560
Himanshu Sharma <97234895+himanshu-901@users.noreply.github.com>
2022-09-01 16:31:05 +0530

    Create manually.cpp

---
+++ Lecture 7: Switch & functions/functions/manually.cpp
@@ -0,0 +1,21 @@
+// power of (a,b) - manually
+
+
+/*
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b;
+  cin>>a>>b;
+  int ans = 1;
+  for(int i=1;i<=b;i++){
+    ans = ans*a;
+  }
+  cout<<ans<<endl;
+  return 0;
+}
+
+
+*/
