89783c567dbfc20c5b2b36bbbc885edbc8c954d9
Himanshu Sharma <97234895+himanshu-901@users.noreply.github.com>
2022-09-01 16:34:56 +0530

    Create counting.cpp

---
+++ Lecture 7: Switch & functions/functions/counting.cpp
@@ -0,0 +1,20 @@
+// print counting using functions 
+
+/*
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void count(int n){
+  for(int i=1;i<=n;i++){
+    cout<<i<<" ";
+  }
+}
+
+int main(){
+  int n;
+  cin>>n;
+  count(n);
+}
+
+*/
