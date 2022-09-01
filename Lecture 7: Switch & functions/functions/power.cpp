84349423abe8fc1ef0cf8883b0dd9e125cfeb992
Himanshu Sharma <97234895+himanshu-901@users.noreply.github.com>
2022-09-01 16:31:56 +0530

    Create power.cpp

---
+++ Lecture 7: Switch & functions/functions/power.cpp
@@ -0,0 +1,26 @@
+// pow of (a,b) - using functions
+
+
+/*
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//function
+int pow(){
+  int a,b;
+  cin>>a>>b;
+  int ans = 1;
+  for(int i=1;i<=b;i++){
+    ans = ans * a;
+  }
+  return ans;
+}
+
+int main (){
+  int ans = pow();
+  cout<<ans;
+}
+
+
+*/
