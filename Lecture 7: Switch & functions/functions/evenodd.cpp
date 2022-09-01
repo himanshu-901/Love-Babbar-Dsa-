e3b02a18176298b2b7c5651e50c199c0a9f2bee9
Himanshu Sharma <97234895+himanshu-901@users.noreply.github.com>
2022-09-01 16:32:54 +0530

    Create evenodd.cpp

---
+++ Lecture 7: Switch & functions/functions/evenodd.cpp
@@ -0,0 +1,29 @@
+// program for even and odd
+
+
+/*
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int evenodd(int x){
+  //even
+  if(x%2==0){
+    return 0;
+  }
+    //odd
+  else{
+    return 1;
+  }
+}
+
+
+int main(){
+  int num;
+  cin>>num;
+  int ans = evenodd(num);
+  cout<<ans;
+  }
+  
+*/
