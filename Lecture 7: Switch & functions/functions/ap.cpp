de455155bfbb67022e213df7fb2ab81a301f571a
Himanshu Sharma <97234895+himanshu-901@users.noreply.github.com>
2022-09-02 21:32:35 +0530

    Create ap.cpp

---
+++ Lecture 7: Switch & functions/functions/ap.cpp
@@ -0,0 +1,19 @@
+//homework questions 
+
+// Arithmetic Progression - (AP)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int ap(int n){
+  int ans = (3*n)+7;
+  return ans;
+}
+
+int main() 
+{
+  int n;
+  cin>>n;
+  cout<<ap(n);
+    return 0;
+}
