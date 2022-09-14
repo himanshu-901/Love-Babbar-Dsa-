f0cc2ee180812b9f83fa155df30a6e86a2f78d72
Himanshu Sharma <97234895+himanshu-901@users.noreply.github.com>
2022-09-14 08:44:47 +0530

    Create swap_alternate.cpp

---
+++ Lecture 10 : array Question/swap_alternate.cpp
@@ -0,0 +1,33 @@
+// swap alternate 
+/*
+
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[],int n){
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+}
+
+void swap(int arr[],int size){
+  for(int i=0;i<size;i=i+2){
+    if(i+1<size){
+      swap(arr[i],arr[i+1]);
+    }
+  }
+}
+
+int main() {
+  int even[6] = {2,5,6,7,9,1};
+  int odd[5]={1,3,5,7,9};
+
+
+  swap(even,6);
+  printArray(even, 6);
+
+  swap(odd,5);
+  printArray(odd, 5);
+  }
+*/
